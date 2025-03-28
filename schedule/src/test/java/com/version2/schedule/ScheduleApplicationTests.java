578afba69bf359615e5747a3c068ef69e9f240f3
MJK-One <hanleejss012@naver.com>
2025-03-28 16:16:05 +0900

    Project Setting

---
+++ schedule/src/test/java/com/version2/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.version2.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
